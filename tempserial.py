a20fb6b81ba5fd0071a5dfd79ad1fc0fd53adb90
Harrison Kyle <ve2hkw@gmail.com>
2015-01-28 17:44:46 -0500

    Initial commit, concider it v0.1

---
+++ tempserial.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  tempserial.py
+#  
+#  Copyright 2015 user <user@user-HP-Compaq-nc6320-EN186UT-ABA>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import time
+import serial
+import requests
+from datetime import datetime
+import os
+
+def main():
+	print('Hi there, please choose one of the following ports:')
+	print('1- /dev/ttyACM0\n2- /dev/ttyACM1\n3- /dev/ttyS0\nENTER for manual entry')
+	portnumber = raw_input("$: ")
+	try:
+		if portnumber == '1':
+			serialport = '/dev/ttyACM0'
+			print('Selected /dev/ttyACM0')
+		elif portnumber == '2':
+			serialport = '/dev/ttyACM1'
+			print('Selected /dev/ttyACM1')
+		elif portnumber == '3':
+			serialport = '/dev/ttyS0'
+			print('Selected /dev/ttyS0')
+		elif not portnumber:
+			serialport = str(raw_input("$: "))
+			if "/dev/" not in serialport:
+				raise NameError("Invalid serial port, exiting")
+			print('Selected ' + serialport)
+		else:
+			print("Invalid entry")
+			
+	except:
+		print("You likely typed something wrong")
+		time.sleep(1)
+	
+	internetQuery = raw_input("Do you want internet logging? (Y/n) $:")
+	if internetQuery == "y":
+		internet = 1
+	elif internetQuery == "Y":
+		internet = 1
+	elif internetQuery == "n":
+		internet = 0
+	elif internetQuery == "N":
+		internet = 0
+	elif not internetQuery:
+		internet = 1
+	else:
+		print("You didn't enter stuff correctly, assuming yes")
+		internet = 1
+	folder_setup()
+	readtemp(serialport, internet)
+	
+def readtemp(port, internet):
+	serialport = serial.Serial(port, 9600)
+	indata = ""
+	while "READY" not in indata:		
+		 indata = serialport.readline()
+		 print(indata)
+		 time.sleep(0.5)
+	filename = open("datalog/temperature " + datetime.utcnow().ctime() + ".log.csv", 'w')
+	filename.write("Time, Indoor temp, Outdoor temp\n")
+	print("accepting data now")
+	while True:
+		serialport.write("t") # used to trigger the arduino temp sensor readings
+		tempdata = serialport.readline()
+		curtime = datetime.utcnow().ctime()
+		
+		localtempdata = str(curtime) + ", " + str(tempdata)
+		
+		cutcurtime = curtime[4:19]
+		
+		splittempdata = tempdata.split(',')
+		
+		outdoortemp = splittempdata[1]
+		indoortemp = splittempdata[0]
+		shortindoortemp = indoortemp[4:]
+		shortoutdoortemp = outdoortemp[5:]
+		
+		#print(cutcurtime + " " + indoortemp + " " + outdoortemp)
+		filename.write(cutcurtime + ", " + shortindoortemp + ", " + shortoutdoortemp)
+		
+		
+		
+		#filename.write(localtempdata)
+		
+		if internet == 1:
+			remotetempdata = {'time': curtime, 'temp': tempdata}
+			httpsend = requests.post("http://kylenet.dyndns.org:8080/temperature", data=remotetempdata)
+#			print(httpsend.text)
+		
+		print(localtempdata)
+		time.sleep(5)
+	
+def folder_setup():
+	if not os.path.isdir('datalog/'):
+		os.makedirs("datalog/")
+		print("loging directory created")
+
+	
+		
+if __name__=='__main__':
+	main()

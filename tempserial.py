93697bcf9df8afc3d625e124360001c8d6ba3b72
Harrison Kyle <ve2hkw@gmail.com>
2015-01-28 18:03:29 -0500

    Added more comments to the main program

--- tempserial.py
+++ tempserial.py
@@ -27,10 +27,14 @@ import requests
 from datetime import datetime
 import os
 
+server = "http://kylenet.dyndns.org:8080/temperature" #webserver you wish to push info to
+sample_time = 5 #how many seconds we want to wait before getting temp data
+
 def main():
-	print('Hi there, please choose one of the following ports:')
+	print('Hi there, please choose one of the following ports:') 
 	print('1- /dev/ttyACM0\n2- /dev/ttyACM1\n3- /dev/ttyS0\nENTER for manual entry')
 	portnumber = raw_input("$: ")
+	#determins the serialport the Arduino is on
 	try:
 		if portnumber == '1':
 			serialport = '/dev/ttyACM0'
@@ -41,9 +45,9 @@ def main():
 		elif portnumber == '3':
 			serialport = '/dev/ttyS0'
 			print('Selected /dev/ttyS0')
-		elif not portnumber:
+		elif not portnumber: #If only the enter key is pressed, then we want different data than supplied by default
 			serialport = str(raw_input("$: "))
-			if "/dev/" not in serialport:
+			if "/dev/" not in serialport: #Needs to change a bit to support non linux operating systems
 				raise NameError("Invalid serial port, exiting")
 			print('Selected ' + serialport)
 		else:
@@ -53,7 +57,7 @@ def main():
 		print("You likely typed something wrong")
 		time.sleep(1)
 	
-	internetQuery = raw_input("Do you want internet logging? (Y/n) $:")
+	internetQuery = raw_input("Do you want internet logging? (Y/n) $:") #we don't want the program to exit if there is not an internet connection so we ask
 	if internetQuery == "y":
 		internet = 1
 	elif internetQuery == "Y":
@@ -67,51 +71,51 @@ def main():
 	else:
 		print("You didn't enter stuff correctly, assuming yes")
 		internet = 1
-	folder_setup()
+	folder_setup() #check for, and make if it isn't there, a folder for the logger data
 	readtemp(serialport, internet)
 	
 def readtemp(port, internet):
 	serialport = serial.Serial(port, 9600)
 	indata = ""
-	while "READY" not in indata:		
+	while "READY" not in indata: #the arduino code needs to init the sensors, once the text "READY" we are ready to go
 		 indata = serialport.readline()
 		 print(indata)
 		 time.sleep(0.5)
 	filename = open("datalog/temperature " + datetime.utcnow().ctime() + ".log.csv", 'w')
-	filename.write("Time, Indoor temp, Outdoor temp\n")
+	filename.write("Time, Indoor temp, Outdoor temp\n") #makes processing this into a graph a lot easier
 	print("accepting data now")
 	while True:
 		serialport.write("t") # used to trigger the arduino temp sensor readings
 		tempdata = serialport.readline()
-		curtime = datetime.utcnow().ctime()
+		curtime = datetime.utcnow().ctime() #current time UTC
 		
 		localtempdata = str(curtime) + ", " + str(tempdata)
 		
+		#text formating starts here
 		cutcurtime = curtime[4:19]
-		
 		splittempdata = tempdata.split(',')
-		
 		outdoortemp = splittempdata[1]
 		indoortemp = splittempdata[0]
 		shortindoortemp = indoortemp[4:]
 		shortoutdoortemp = outdoortemp[5:]
+		#text formating ends about here
 		
 		#print(cutcurtime + " " + indoortemp + " " + outdoortemp)
-		filename.write(cutcurtime + ", " + shortindoortemp + ", " + shortoutdoortemp)
+		filename.write(cutcurtime + ", " + shortindoortemp + ", " + shortoutdoortemp) #writes the temp data to the log file
 		
 		
 		
 		#filename.write(localtempdata)
 		
-		if internet == 1:
+		if internet == 1: #if we turned on internet, we want to push to a server
 			remotetempdata = {'time': curtime, 'temp': tempdata}
-			httpsend = requests.post("http://kylenet.dyndns.org:8080/temperature", data=remotetempdata)
+			httpsend = requests.post(server, data=remotetempdata)
 #			print(httpsend.text)
 		
-		print(localtempdata)
-		time.sleep(5)
+		print(localtempdata) #prints temp data to local output
+		time.sleep(sample_time)
 	
-def folder_setup():
+def folder_setup(): #checks for the datalog folder, and if it isn't there, make it
 	if not os.path.isdir('datalog/'):
 		os.makedirs("datalog/")
 		print("loging directory created")
